9d734fcd0696c1d60d86901c75ecbc2161db255c
Emmanuel Jaramillo Restrepo <ejaramillor@tcc.com.co>
2023-11-12 09:33:17 -0500

    Creacion de servicios

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeormService } from './services/typeorm/typeorm.service';
+import { ClientesService } from './services/clientes/clientes.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, TypeormService, ClientesService],
 })
 export class AppModule {}
