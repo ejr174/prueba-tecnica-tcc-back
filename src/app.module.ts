b3fc939ea4d1aa1420944b2bdbdc94235d57d813
Emmanuel Jaramillo Restrepo <ejaramillor@tcc.com.co>
2023-11-12 09:35:02 -0500

    Creacion del modelo y controlador

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeormService } from './services/typeorm/typeorm.service';
 import { ClientesService } from './services/clientes/clientes.service';
+import { ClientesController } from './controller/clientes/clientes.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, ClientesController],
   providers: [AppService, TypeormService, ClientesService],
 })
 export class AppModule {}
