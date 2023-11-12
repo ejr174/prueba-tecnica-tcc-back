93b299678d0a9db63f47a58053248c28ed4fc4a8
Emmanuel Jaramillo Restrepo <ejaramillor@tcc.com.co>
2023-11-12 12:40:09 -0500

    Se crearon las funciones del servicio

--- src/services/clientes/clientes.service.ts
+++ src/services/clientes/clientes.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Clientes } from 'src/models/clientes.model';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class ClientesService {}
+export class ClientesService {
+    constructor(
+        @InjectRepository(Clientes) private clientesRepository : Repository<Clientes>
+    ){}
+
+    async findAll() : Promise<Clientes[]>{
+        return await this.clientesRepository.find()
+    }
+
+    async create(cliente : Clientes) : Promise<Clientes>{
+        return await this.clientesRepository.save(cliente)
+    }    
+
+    async update(cliente : Clientes) : Promise<Clientes>{
+        return await this.clientesRepository.save(cliente)
+    }        
+
+    async delete(id : number) : Promise<string>{
+        await this.clientesRepository.delete(id)
+
+        return 'OK'
+    }        
+}
