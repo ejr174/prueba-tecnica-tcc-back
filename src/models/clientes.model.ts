5126dea37d8704358de87a328f186d71846a4809
Emmanuel Jaramillo Restrepo <ejaramillor@tcc.com.co>
2023-11-12 15:17:27 -0500

    ajuste molo clientes

--- src/models/clientes.model.ts
+++ src/models/clientes.model.ts
@@ -1,13 +1,21 @@
-import { Entity, PrimaryColumn } from "typeorm";
+import { Column, Entity, PrimaryColumn } from "typeorm";
 
 @Entity()
 export class Clientes {
     @PrimaryColumn()
+    clienteID : number;
+    @Column()
     numeroIdentificacion : string;
+    @Column()
     nombre : string;
+    @Column()
     correoElectronico : string;
+    @Column()
     telefono : string;
+    @Column()
     fechaRegistro : string;
+    @Column()
     tipoIdentificacionID : number;
+    @Column()
     generoID : number   ;
 }
