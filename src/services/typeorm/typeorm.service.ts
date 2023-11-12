71c910a10c1618d20b91f2b21ecec34703dad473
Emmanuel Jaramillo Restrepo <ejaramillor@tcc.com.co>
2023-11-12 09:56:18 -0500

    Conexion inicial base de datos

--- src/services/typeorm/typeorm.service.ts
+++ src/services/typeorm/typeorm.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm/dist';
 
 @Injectable()
-export class TypeormService {}
+export class TypeormService implements TypeOrmOptionsFactory {
+    createTypeOrmOptions(): TypeOrmModuleOptions | Promise<TypeOrmModuleOptions> {
+      return  {
+        type : 'mssql',
+        host : 'SQL_Latin1_General_CP1_CI_AS',
+        username : 'TCC\ejaramillor',
+        password : '',
+        port: 1433,
+        database : 'dbPruebaTecnica',
+
+        entities : ['dist/**/*.model.{ts.js}']
+      }
+    }
+}
